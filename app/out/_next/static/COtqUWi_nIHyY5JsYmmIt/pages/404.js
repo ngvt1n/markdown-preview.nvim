e398f3b0b977d036ae2ce08076f65fcb06888eda
Tin Nguyen <nguyenvb.tin03@gmail.com>
2025-11-25 23:16:56 -0500

    chore: build after adding attributes

---
+++ app/out/_next/static/COtqUWi_nIHyY5JsYmmIt/pages/404.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{439:function(n,e,t){__NEXT_REGISTER_PAGE("/404",function(){return n.exports=t(440),{page:n.exports.default}})},440:function(n,e,t){"use strict";t.r(e);var u=t(1),o=t.n(u);e.default=function(){return o.a.createElement("div",null,"404")}}},[[439,1,0]]]);
